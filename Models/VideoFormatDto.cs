40644bacbc02c88e234e72793b06dcad6d141e41
sshamsudin558-sketch <sshamsudin558@gmail.com>
2026-08-23 23:42:52 -0700

    Add VideoFormatDto.cs

--- Models/VideoFormatDto.cs
+++ Models/VideoFormatDto.cs
@@ -1,1 +1,14 @@
+namespace VideoDownloader.Web.Models;
 
+public class VideoFormatDto
+{
+    public string FormatId { get; set; } = string.Empty;
+
+    public string Extension { get; set; } = string.Empty;
+
+    public string Resolution { get; set; } = string.Empty;
+
+    public string FileSizeFormatted { get; set; } = string.Empty;
+
+    public bool IsAudioOnly { get; set; }
+}
