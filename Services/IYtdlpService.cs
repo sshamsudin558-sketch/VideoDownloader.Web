4a758f374a1f7674d73be258858740c43d5c7ad0
sshamsudin558-sketch <sshamsudin558@gmail.com>
2026-08-23 23:34:20 -0700

    Add IYtdlpService.cs

--- Services/IYtdlpService.cs
+++ Services/IYtdlpService.cs
@@ -1,1 +1,12 @@
+using VideoDownloader.Web.Models;
 
+namespace VideoDownloader.Web.Services;
+
+public interface IYtdlpService
+{
+    Task<VideoInfoDto?> GetVideoInfoAsync(string url);
+
+    Task<string?> DownloadAsync(
+        string url,
+        string formatId);
+}
