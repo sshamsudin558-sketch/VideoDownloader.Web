00242f32ec2d57d16847bddb33e72fe39a161771
sshamsudin558-sketch <sshamsudin558@gmail.com>
2026-08-23 23:49:57 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using VideoDownloader.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
+builder.Services.AddControllers();
+
+// Application services
+builder.Services.AddScoped<IYtdlpService, YtdlpService>();
+
+// Automatic download cleanup
+builder.Services.AddHostedService<DownloadCleanupService>();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger only in Development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// HTTPS
+app.UseHttpsRedirection();
+
+// Static files
+app.UseStaticFiles();
+
+// Security headers
+app.Use(async (context, next) =>
+{
+    context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+    context.Response.Headers["X-Frame-Options"] = "SAMEORIGIN";
+    context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
+
+    await next();
+});
+
+// Authorization
+app.UseAuthorization();
+
+// API
+app.MapControllers();
+
+// Frontend
+app.MapFallbackToFile("index.html");
+
+app.Run();
